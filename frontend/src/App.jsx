59de0b920f72b1e830d3b755e0d8f7f98b3c6a72
charaf marghin <developermarghin@gmail.com>
2023-01-02 14:08:53 +0100

    add initial frontend files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<h1>About</h1>} />
+        <Route path="/" element={<h1>Dashboard</h1>} />
+        <Route path="/apartments" element={<h1>Apartments</h1>} />
+        <Route path="/apartments/new" element={<h1>add apartment</h1>} />
+        <Route path="/tenants" element={<h1>Tenants</h1>} />
+        <Route path="/tenants/new" element={<h1>add tenant</h1>} />
+        <Route path="/tenants/:id" element={<h1>tenant details</h1>} />
+
+        <Route path="/rents" element={<h1>Rents</h1>} />
+        <Route path="/rents/new" element={<h1>new rent</h1>} />
+        <Route path="/rents/:id" element={<h1>rent detail</h1>} />
+
+        <Route path="*" element={<h1>404</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
