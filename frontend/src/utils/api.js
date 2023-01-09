7ebfc3a1669f50ac39f08c40f0d29020ab2c818b
charaf marghin <developermarghin@gmail.com>
2023-01-09 18:18:43 +0100

    add apartments CRUD api functionalities to utils

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -14,4 +14,25 @@ const verify = async () => {
   return response.data;
 };
 
-export { login, verify };
+// ================ Apartments ==================
+const getAllApartments = async () => {
+  const response = await axios.get('/apartments/');
+  return response.data;
+};
+
+const addApartment = async (data) => {
+  const response = await axios.post('/apartments', data);
+  return response.data;
+};
+
+const updateApartment = async (id, data) => {
+  const response = await axios.put(`/apartments/${id}`, data);
+  return response.data;
+};
+
+const deleteApartment = async (id) => {
+  const response = await axios.delete(`/apartments/${id}`);
+  return response.data;
+};
+
+export { login, verify, getAllApartments, addApartment, updateApartment, deleteApartment };
